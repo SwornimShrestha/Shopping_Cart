2e21586a3239fee493902981d04f3f9a5262f724
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-04-17 23:33:00 +0545

    updated

--- src/components/khaltiKey.js
+++ src/components/khaltiKey.js
@@ -1,8 +1,6 @@
 let myKey = {
-  publicTestKey: "test_public_key_48ebb470b4774cc3885c38db2d74ad46",
-  secretKey: "test_secret_key_2f5a12496d894f4d834bcf2de62f0e51",
-  //   publicTestKey: "test_public_key_6a04493a53d7479b85f844a6bf74bc0a",
-  //   secretKey: "test_secret_key_b260606bd179430290e66b19c5f56006",
+  publicTestKey: process.env.PUBLIC_TEST_KEY,
+  secretKey: process.env.SECRET_KEY,
 };
 
 export default myKey;
