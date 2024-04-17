7062f25b774074af5a10763a43773b9a309139f7
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-04-18 01:12:55 +0545

    banner

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { PRODUCTS } from "../../products";
 import { Product } from "./product";
+
+import Banner from "../../assets/products/banner.png";
+
 import "./shop.css";
 export const Shop = () => {
   return (
     <div className="shop">
+      <img
+        src={Banner}
+        alt="Banner"
+        style={{ marginTop: "70px", width: "100%", height: "300px" }}
+      />{" "}
+      {/* Image with width and height */}
       <div className="shopTitle">
         <strong>Gharra</strong>
         <small>.Np</small>
