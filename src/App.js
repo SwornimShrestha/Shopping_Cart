6a557cd1f44e5c015a96ba4888814b99ee9968de
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-04-18 00:11:34 +0545

    design

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Shop } from "./pages/shop/shop";
 import { Contact } from "./pages/contact";
 import { Cart } from "./pages/cart/cart";
 import { ShopContextProvider } from "./context/shop-context";
+import Footer from "./components/footer";
 
 function App() {
   return (
@@ -13,12 +14,12 @@ function App() {
       <ShopContextProvider>
         <Router>
           <Navbar />
-
           <Routes>
             <Route path="/contact" element={<Contact />} />
             <Route exact path="/" element={<Shop />} />
             <Route path="/cart" element={<Cart />}></Route>
           </Routes>
+          <Footer />
         </Router>
       </ShopContextProvider>
     </div>
---
+++ src/components/footer.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./footer.css"; // Import the CSS file
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="contain">
+        <div className="col">
+          <h1>Company</h1>
+          <ul>
+            <li>About</li>
+            <li>Services</li>
+            <li>Get in touch</li>
+          </ul>
+        </div>
+        <div className="col">
+          <h1>Products</h1>
+          <ul>
+            <li>About</li>
+            <li>Mission</li>
+            <li>Services</li>
+            <li>Social</li>
+            <li>Get in touch</li>
+          </ul>
+        </div>
+        <div className="col">
+          <h1>Support</h1>
+          <ul>
+            <li>Contact us</li>
+            <li>Web chat</li>
+            <li>Open ticket</li>
+          </ul>
+        </div>
+        <div className="col social">
+          <h1>Social</h1>
+          <ul>
+            <li>
+              <img
+                src="https://svgshare.com/i/5fq.svg"
+                alt="social-icon"
+                width="32"
+              />
+            </li>
+            <li>
+              <img
+                src="https://svgshare.com/i/5eA.svg"
+                alt="social-icon"
+                width="32"
+              />
+            </li>
+            <li>
+              <img
+                src="https://svgshare.com/i/5f_.svg"
+                alt="social-icon"
+                width="32"
+              />
+            </li>
+          </ul>
+        </div>
+        <div className="clearfix"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
