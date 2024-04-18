fd93e98f00d2bef92db75268cb1ca061433af70f
Swornim Shrestha <swornimshrestha639@gmail.com>
2024-04-18 09:29:40 +0545

    search

--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,9 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ShopContext } from "../../context/shop-context";
 
 export const Product = (props) => {
   const { id, productName, price, productImage } = props.data;
   const { addToCart, cartItems } = useContext(ShopContext);
+  const [search, setSearch] = useState("");
 
   const cartItemCount = cartItems[id];
 
@@ -14,7 +15,7 @@ export const Product = (props) => {
         <p>
           <b>{productName}</b>
         </p>
-        <p> ${price}</p>
+        <p> NPR{price}</p>
       </div>
       <button className="addToCartBttn" onClick={() => addToCart(id)}>
         Add To Cart {cartItemCount > 0 && <> ({cartItemCount})</>}
--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -6,28 +6,51 @@ import Banner from "../../assets/products/banner.png";
 import Banner1 from "../../assets/products/banner1.png";
 
 import "./shop.css";
+
 export const Shop = () => {
+  const [search, setSearch] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState(PRODUCTS);
+
+  const handleSearch = () => {
+    const filtered = PRODUCTS.filter(
+      (product) =>
+        product.productName.toLowerCase().includes(search.toLowerCase()) ||
+        product.id.toString() === search
+    );
+    setFilteredProducts(filtered);
+  };
+
   return (
     <div className="shop">
       <img
         src={Banner}
         alt="Banner"
-        style={{ marginTop: "70px", width: "100%", height: "300px" }}
-      />{" "}
+        style={{ marginTop: "70px", width: "90%", height: "300px" }}
+      />
       {/* Image with width and height */}
       <img
         src={Banner1}
         alt="Banner"
-        style={{ marginTop: "70px", width: "100%", height: "150px" }}
-      />{" "}
+        style={{ marginTop: "70px", width: "90%", height: "150px" }}
+      />
       {/* Image with width and height */}
       <div className="shopTitle">
-        <strong>Gharra</strong>
+        <strong> Welcome to Gharra</strong>
         <small>.Np</small>
       </div>
-      {/* <p>{loggeduser ? loggeduser[0].email:"No data"}</p> */}
+      <div>
+        <input
+          value={search}
+          className="searchInput"
+          placeholder="search here"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button className="search-btn" onClick={handleSearch}>
+          Search
+        </button>
+      </div>
       <div className="products">
-        {PRODUCTS.map((product) => (
+        {filteredProducts.map((product) => (
           <Product data={product} key={product.id} />
         ))}
       </div>
